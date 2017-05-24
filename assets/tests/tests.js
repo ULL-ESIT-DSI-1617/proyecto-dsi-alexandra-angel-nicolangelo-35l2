6c4f5ee6865b9813a2294c8c7c97ffcb00e11e9d
wiwihyuga <angel_luis_tf@hotmail.com>
2017-05-24 11:49:12 +0000

    añadiendo test

--- assets/tests/tests.js
+++ assets/tests/tests.js
@@ -1,19 +1,19 @@
 var assert = chai.assert;
 
 suite('temperature', function() {
-    test('32F = 0C', function() {
-        original.value = "32F";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+    test('32F = 0 Celsius', function() {
+        convert.value = "32F to C";
+        main();
+        assert.deepEqual(converted.innerHTML, "0 Celsius");
     });
     test('45C = 113.0 Farenheit', function() {
-        original.value = "45C";
-        calculate();
+        convert.value = "45C F";
+        main();
         assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
     });
     test('5X = error', function() {
-        original.value = "5X";
-        calculate();
+        convert.value = "5X";
+        main();
         assert.match(converted.innerHTML, /ERROR/);
     });
 });
