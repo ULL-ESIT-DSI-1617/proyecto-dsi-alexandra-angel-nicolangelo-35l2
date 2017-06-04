40764924d0f19a5fb96653ae0006427bd63ef351
wiwihyuga <angel_luis_tf@hotmail.com>
2017-06-04 21:39:43 +0000

    travis 5

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var assert = chai.assert;
+
+suite('temperature', function() {
+    test('32F = 0 Celsius', function() {
+        convert.value = "32F to C";
+        main();
+        assert.deepEqual(converted.innerHTML, "0 Celsius");
+    });
+    test('45C = 113.0 Farenheit', function() {
+        convert.value = "45C to F";
+        main();
+        assert.deepEqual(converted.innerHTML, "113 Farenheit");
+    });
+    test('5X = error', function() {
+        convert.value = "5X to F";
+        main();
+        assert.match(converted.innerHTML, /Desconozco como convertir desde "x" hasta "f"/);
+    });
+});
