a652f670e424adc944110d873c6bcc84613c7f20
wiwihyuga <angel_luis_tf@hotmail.com>
2017-06-04 17:55:31 +0000

    pruebas mocha y chai

--- assets/tests/tests.js
+++ assets/tests/tests.js
@@ -1,19 +1,19 @@
 var assert = chai.assert;
 
 suite('temperature', function() {
-    test('32F = 0C', function() {
-        original.value = "32F";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+    test('32F = 0 Celsius', function() {
+        convert.value = "32F to C";
+        main();
+        assert.deepEqual(converted.innerHTML, "0 Celsius");
     });
     test('45C = 113.0 Farenheit', function() {
-        original.value = "45C";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+        convert.value = "45C to F";
+        main();
+        assert.deepEqual(converted.innerHTML, "113 Farenheit");
     });
     test('5X = error', function() {
-        original.value = "5X";
-        calculate();
-        assert.match(converted.innerHTML, /ERROR/);
+        convert.value = "5X to F";
+        main();
+        assert.match(converted.innerHTML, /Desconozco como convertir desde "x" hasta "f"/);
     });
 });
