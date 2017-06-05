ef8e0ff5ba6bc0e88203cf2819a0abc596850803
wiwihyuga <angel_luis_tf@hotmail.com>
2017-06-05 17:35:11 +0000

    esperanza

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var chai = require('chai');
 var assert = chai.assert;
 var convert = "32F to C";
-var mainn = "../assets/js/main.js";
-var temperature = "../assets/js/temperature.js";
-var medida = "../assets/js/medida.js";
+var main = require('../assets/js/main.js');
+var temperature = require('../assets/js/temperature.js');
+var medida =require('../assets/js/medida.js');
 
 suite('temperature', function() {
     test('32F = 0 Celsius', function() {
