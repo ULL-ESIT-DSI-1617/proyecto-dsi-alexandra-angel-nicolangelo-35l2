f0f8d6433269c3843764b1a6d9a7df449acd9304
Alexandra <alexandraesplugas07@gmail.com>
2017-07-01 18:43:29 +0000

    Cambiando estilo del proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 (function (exports) {
     "use strict";
     function main() {
@@ -5,13 +6,9 @@
         var valor = document.getElementById('convert').value,
             elemento = document.getElementById('converted');
         elemento.innerHTML = Medida.convertir(valor);
-
-        return false;
-
         //enviamos al index la temperatura final para su inclusion en la base de datos
         t_convertida = document.getElementById("estesi").value = Medida.convertir(valor);
         return t_convertida;
-
     }
     exports.main = main;
 })(this);
